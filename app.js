22e21863fea1260dadcb1efffe60b0c636fd36c2
Justin Singleton <justin.singleton.dev@gmail.com>
2021-06-11 22:04:31 -0700

    testing server

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 window.onload = function loadDoc() {
     xhttp = new XMLHttpRequest();
-    json = this.responseText
-    txt = "";
-    j = JSON.parse(json);
-
-    schools = j.Schools;
-
-    alert (schools);
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            for (i = 0; i < schools.length; i++) {
-                txt = txt + schools[i].childNodes[0].nodeValue + "<br>";
-            }
-            document.getElementById("demo").innerHTML = txt;
+            json = this.responseText;
+            txt = "";
+            j = JSON.parse(json);
+
+            schools = j.Schools;
+
+            alert (schools);
         }
     };
     xhttp.open("GET", "schools.json", true);
     xhttp.send();
 }
+
+
+//xhttp.onreadystatechange = function() {
+//    if (this.readyState == 4 && this.status == 200) {
+//        for (i = 0; i < schools.length; i++) {
+//            txt = txt + schools[i].childNodes[0].nodeValue + "<br>";
+//        }
+//        document.getElementById("demo").innerHTML = txt;
+//    }
+//};
