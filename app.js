82a73cebd3ebf0e626368c46c2f36e4409198d6b
Justin Singleton <justin.singleton.dev@gmail.com>
2021-06-11 21:00:36 -0700

    testing server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.onload = function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
+            document.getElementById("demo").innerHTML = this.my_schools;
         }
     };
     xhttp.open("GET", "schools.json", true);
