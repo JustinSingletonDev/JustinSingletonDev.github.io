97af284b9928f14e568c888fe34fdf86669e91e5
Justin Singleton <justin.singleton.dev@gmail.com>
2021-06-12 22:17:28 -0700

    testing rows

--- app.js
+++ app.js
@@ -8,15 +8,25 @@ window.onload = function loadDoc() {
             txt = "";
             j = JSON.parse(json);
             
-            for (const property in j) {
-                document.getElementById("titles").insertAdjacentHTML(
-                    "beforeend", `<th>${property}</th>`);
-                console.log(`<th>${property}</th>`);
-            }
             for (const property in j.CCAF) {
                 document.getElementById("CCAF").insertAdjacentHTML(
                     "beforeend", `<td>${j.CCAF[property]}</td>`);
                 console.log(`${j.CCAF[property]}`);
+            }
+            for (const property in j.DLI) {
+                document.getElementById("DLI").insertAdjacentHTML(
+                    "beforeend", `<td>${j.DLI[property]}</td>`);
+                console.log(`${j.DLI[property]}`);
+            }
+            for (const property in j.Bellevue) {
+                document.getElementById("Bellevue").insertAdjacentHTML(
+                    "beforeend", `<td>${j.Bellevue[property]}</td>`);
+                console.log(`${j.Bellevue[property]}`);
+            }
+            for (const property in j.Boston) {
+                document.getElementById("Boston").insertAdjacentHTML(
+                    "beforeend", `<td>${j.Boston[property]}</td>`);
+                console.log(`${j.Boston[property]}`);
             }
         }
     };
